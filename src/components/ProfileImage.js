93e5e4ea07e97421e88a60bf8a0cc5b11bfd69a4
Marcel Michau <marcelmichau@gmail.com>
2019-01-25 19:21:40 +0200

    fix (profile image): Fix gatsby-image style bug on Safari (#87)
    Workaround styles applying late on gatsby-image on Safari on iOS/macOS

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 
-const StyledImage = styled(Img)`
-	border-radius: 50%;
-`;
-
 const ProfileImage = ({ fixed }) => {
-	return <StyledImage fixed={fixed} alt="Profile Picture of Marcel Michau" />;
+	return (
+		<Img
+			imgStyle={{ borderRadius: '50%' }}
+			fixed={fixed}
+			alt="Profile Picture of Marcel Michau"
+		/>
+	);
 };
 
 ProfileImage.propTypes = {
