22745665646d2183b8b1311182be7c9aa8e9861b
Marcel Michau <marcelmichau@gmail.com>
2019-01-25 18:05:49 +0200

    feat (animations & styles): Some refinements (#86)
    Add some animations to header & main content using react-spring
    Style links differently
    Update README & main-content.md with updated tools & services
    Set prettier format command to use tabs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Spring } from 'react-spring';
 import Layout from '../components/Layout';
 import PageContainer from '../components/PageContainer';
 import ProfileImage from '../components/ProfileImage';
@@ -9,14 +10,32 @@ export function IndexPage({ data }) {
 		<Layout>
 			<PageContainer
 				headerContent={
-					<div>
-						<h2>Marcel Michau</h2>
-						<ProfileImage fixed={data.file.childImageSharp.fixed} />
-						<p>I write code & stuff</p>
-					</div>
+					<Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+						{({ opacity }) => (
+							<div style={{ opacity }}>
+								<h2>Marcel Michau</h2>
+								<ProfileImage fixed={data.file.childImageSharp.fixed} />
+								<p>I write code & stuff</p>
+							</div>
+						)}
+					</Spring>
 				}
 				mainContent={
-					<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+					<Spring
+						delay={1000}
+						from={{ opacity: 0, y: '40px' }}
+						to={{ opacity: 1, y: '0px' }}
+					>
+						{({ opacity, y }) => (
+							<div
+								style={{
+									opacity,
+									transform: `translate3d(0, ${y}, 0)`
+								}}
+								dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+							/>
+						)}
+					</Spring>
 				}
 			/>
 		</Layout>
