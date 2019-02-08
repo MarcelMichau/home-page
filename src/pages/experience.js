7c887e02824c98ce64f74fbfbd81bc7b57ddca86
Marcel Michau <marcelmichau@gmail.com>
2019-02-08 20:14:35 +0200

    fix(Tests & Routes): Trim duplicate code in tests Wrap other pages with Layout component & ThemeProvider

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import PageContainer from '../components/PageContainer';
+import theme from '../styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 const Experience = () => {
 	return (
-		<Layout>
-			<PageContainer
-				headerContent={<div>Some header</div>}
-				mainContent={<div>Some experience content</div>}
-			/>
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<PageContainer
+					headerContent={<div>Some header</div>}
+					mainContent={<div>Some experience content</div>}
+				/>
+			</Layout>
+		</ThemeProvider>
 	);
 };
 
