5c3a63d443cd43599e231dde521788b996acc0b0
Marcel Michau <marcelmichau@gmail.com>
2019-04-09 14:19:16 +0200

    refactor(timer): split out Timer component

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { differenceInSeconds } from 'date-fns';
+import React from 'react';
+import Timer from './Timer';
 
 const sinceDate = new Date(2014, 1, 15, 8, 0, 0);
 
 const About = () => {
-	const [now, updateNow] = useState(differenceInSeconds(new Date(), sinceDate));
-
-	useEffect(() => {
-		const timerID = setInterval(() => tick(), 1000);
-
-		return function cleanup() {
-			clearInterval(timerID);
-		};
-	});
-
-	function tick() {
-		updateNow(differenceInSeconds(new Date(), sinceDate));
-	}
-
 	return (
 		<div>
 			<h1>About Me</h1>
 
 			<p>I have been a professional software developer for approximately:</p>
-			<p>{now.toLocaleString()} seconds</p>
+			<p>
+				<Timer sinceDate={sinceDate} /> seconds
+			</p>
 
 			<p>But who's counting. ;)</p>
 
