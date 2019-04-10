42aed3f57850e39c2883d5e0d1f57542c2c61fb9
Marcel Michau <marcelmichau@gmail.com>
2019-04-10 06:09:40 +0200

    Feat/cv (#218)
    * feat(cv): add initial cv page
    
    * feat(cv): fleshed out cv page
    
    * feat(cv): add headings
    
    * feat(cv): fleshed out CV with updated info
    
    * feat(cv): testing markdown
    
    * fix(cv): remove About import
    
    * fix(imports): fix imports in cv page
    
    * refactor(structure): remove some duplication
    
    * refactor(animation): change animation util to custom hook
    
    * fix(profile): move useSpring hook into component
    
    * refactor(timer): split out Timer component
    
    * refactor(cv): split about into separate components
    
    * refactor(cv): make ul lists render from arrays
    
    * refactor(lists): create ItemList component for lists
    
    * fix(item list): copy-paste issue
    
    * fix(cv): typos & grammar
    
    * fix(cv): refinements
    
    * fix(experience): fix long word preventing wrapping
    
    * fix(cv): more refinements
    
    * feat(cv): add link to main page at bottom
    
    * feat(cv): added some skills & experience
    added skills legend

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,6 +7,7 @@ const skills = [
 	['TypeScript', 'Intermediate'],
 	['CSS', 'Basic'],
 	['HTML', 'Intermediate'],
+	['SQL', 'Intermediate'],
 	['.NET Framework', 'Intermediate'],
 	['.NET Core', 'Intermediate'],
 	['ASP.NET Core', 'Intermediate'],
@@ -18,6 +19,7 @@ const skills = [
 	['Asynchronous Messaging', 'Intermediate'],
 	['IBM MQ', 'Intermediate'],
 	['RabbitMQ', 'Basic'],
+	['gRPC', 'Basic'],
 	['Microsoft Azure', 'Basic'],
 	['Amazon Web Services', 'Basic'],
 	['Azure DevOps', 'Advanced'],
@@ -26,8 +28,10 @@ const skills = [
 	['Docker', 'Intermediate'],
 	['Kubernetes', 'Intermediate'],
 	['Node.JS', 'Intermediate'],
+	['GraphQL', 'Basic'],
 	['MongoDB', 'Basic'],
 	['React.JS', 'Advanced'],
+	['React Native', 'Basic'],
 	['Angular', 'Basic'],
 	['Express', 'Basic'],
 	['Mongoose', 'Basic'],
@@ -39,6 +43,23 @@ const skills = [
 const Skills = () => (
 	<div>
 		<h2>Skills</h2>
+
+		<h4>Legend</h4>
+
+		<ul>
+			<li>
+				Basic - I have played around with it, read about it, but have not used
+				it very often.
+			</li>
+			<li>
+				Intermediate - I have decent knowledge about it, used it in a few
+				projects and am comfortable with it.
+			</li>
+			<li>
+				Advanced - I have extensive knowledge about it, used it in numerous
+				projects and am comfortable enough to give a talk about it.
+			</li>
+		</ul>
 
 		<Table headers={['Area', 'Proficiency']} rows={skills} />
 	</div>
