57edc1a1c2b6082c25bec0a6c36447e93747ab4a
Marcel Michau <marcelmichau@gmail.com>
2019-10-10 17:00:30 +0200

    chore(deps): bump dependencies (#302)
    update yarn.lock file
    update gatsby mock
    update snapshots

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -1,14 +1,26 @@
 const React = require('react');
 const gatsby = jest.requireActual('gatsby');
-
 module.exports = {
 	...gatsby,
 	graphql: jest.fn(),
-	Link: jest.fn().mockImplementation(({ to, ...rest }) =>
-		React.createElement('a', {
-			...rest,
-			href: to
-		})
+	Link: jest.fn().mockImplementation(
+		// these props are invalid for an `a` tag
+		({
+			activeClassName,
+			activeStyle,
+			getProps,
+			innerRef,
+			partiallyActive,
+			ref,
+			replace,
+			to,
+			...rest
+		}) =>
+			React.createElement('a', {
+				...rest,
+				href: to
+			})
 	),
-	StaticQuery: jest.fn()
+	StaticQuery: jest.fn(),
+	useStaticQuery: jest.fn()
 };
