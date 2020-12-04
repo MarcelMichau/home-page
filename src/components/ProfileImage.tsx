969a0e3ef1e9f54193a8416c532041a9f7bb5ceb
Marcel Michau <me@marcelmichau.dev>
2020-12-04 13:33:36 +0200

    chore(styles + tests) exclude index page test from codeclimate as it incorrectly picks up duplicate code tweak colour of profile image border

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -15,7 +15,7 @@ const ProfileImage = ({ fixed }: ProfileImageProps) => {
 				height: '250px',
 				width: '250px',
 				margin: '20px',
-				border: `5px solid ${lighten(0.45, theme.accentColour)}`,
+				border: `5px solid ${lighten(0.2, theme.accentColour)}`,
 				boxShadow: '0px 0px 15px 0px rgba(0,0,0,0.75)',
 			}}
 			fixed={fixed}
