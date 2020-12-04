d755e13c04571cea52c0780108516b51b1ee1c34
Marcel Michau <me@marcelmichau.dev>
2020-12-04 13:12:15 +0200

    chore(favicons) try different plugin for favicon generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,19 +39,14 @@ module.exports = {
 			},
 		},
 		{
-			resolve: `gatsby-plugin-favicon`,
+			resolve: `gatsby-plugin-favicons`,
 			options: {
 				logo: path.join(__dirname, `src`, `images`, `mm-logo.png`),
-				injectHTML: true,
 				icons: {
 					android: true,
 					appleIcon: true,
 					appleStartup: true,
-					coast: false,
 					favicons: true,
-					firefox: true,
-					twitter: true,
-					yandex: false,
 					windows: true,
 				},
 			},
